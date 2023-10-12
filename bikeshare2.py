99acb7b4006c40ddd8ed5fde72b697ff7f05620b
Ebikimi_Moses_Etinkumoh <etinkumohmoses@gmail.com>
2023-10-12 12:36:23 +0100

    Optimized the data filtering process for better efficiency

--- bikeshare2.py
+++ bikeshare2.py
@@ -51,39 +51,21 @@ def get_filters():
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
-
-    Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
+    Optimized the filtering process for better efficiency
     """
-    # load data file into a dataframe
+
     df = pd.read_csv(CITY_DATA[city])
-
-    # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
-
-    # extract month, day of week and Hour from Start Time column to create new columns
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.day_name()
     df['hour'] = df['Start Time'].dt.hour
     
-
-    # filter by month if applicable
     if month != 'all':
-        # use the index of the months list to get the corresponding int
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) + 1
-
-        # filter by month to create the new dataframe
         df = df[df['month'] == month]
 
-    # filter by day of week if applicable
     if day != 'all':
-        
-        # filter by day of week to create the new dataframe
         df = df[df['day_of_week'] == day.title()]
         
     return df
@@ -163,7 +145,7 @@ def user_info(df, city):
         # Display gender counts
         print('Counts of each gender:')
         print(df['Gender'].value_counts())
-        
+
     if 'Birth Year' in df:
         # Display birth year information
         print('Earliest year of birth:', int(df['Birth Year'].min()))
